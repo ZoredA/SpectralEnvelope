6da7228758b027a9797d101dedd2aabc2e6b19d8
Zored <zoredahmer@gmail.com>
2017-12-19 14:25:19 -0500

    initial2

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from appJar import gui
 
 
-app = gui()
+app = gui("Frequency Estimation and Envelope Estimation")
+app.setSticky("news")
+app.setExpand("both")
+app.setFont(20)
+
+app.addButtons(["Open File",  "Generate Wave"], "row=0" )
+
+app.addLabelEntry("Open File")
+
+
 app.addLabel("title", "Welcome to appJar")
 app.setLabelBg("title", "red")
 
